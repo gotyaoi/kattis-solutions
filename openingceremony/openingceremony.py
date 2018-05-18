f835e9814fd2ec907ea96c4e103b69bc3a2ea4a4
gotyaoi <gotyaoi@users.noreply.github.com>
2018-05-18 02:24:04 -0700

    Simplifies the opening ceremony solution.
    The try/catch was a holdover from an earlier solution.

--- openingceremony/openingceremony.py
+++ openingceremony/openingceremony.py
@@ -6,19 +6,16 @@ def main():
     start_index = 0
     count = 0
     mod = 0
-    try:
-        while blocks > 0:
-            count += 1
-            if max_height-mod >= blocks:
-                blocks -= 1
-                max_height_index -= 1
-                max_height = heights[max_height_index]
-            else:
-                mod += 1
-                while start_index <= max_height_index and heights[start_index]-mod == 0:
-                    start_index += 1
-                blocks = max_height_index - start_index + 1
-    except IndexError:
-        pass
+    while blocks > 0:
+        count += 1
+        if max_height-mod >= blocks:
+            blocks -= 1
+            max_height_index -= 1
+            max_height = heights[max_height_index]
+        else:
+            mod += 1
+            while start_index <= max_height_index and heights[start_index]-mod == 0:
+                start_index += 1
+            blocks = max_height_index - start_index + 1
     print(count)
 main()
