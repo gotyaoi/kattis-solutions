e3fbe389a2651a46471972a4be4278c56f5902d2
gotyaoi <gotyaoi@users.noreply.github.com>
2018-05-24 15:52:24 -0700

    Speeds up power strings using a sort of seiving method.

--- powerstrings/powerstrings.py
+++ powerstrings/powerstrings.py
@@ -1,34 +1,40 @@
-import collections
 import sys
-from functools import reduce
-from itertools import product
-from operator import mul
 def prime_factors(n):
     i = 2
     factors = []
+    s = []
     while i * i <= n:
         if n % i:
             i += 1
+            if s:
+                factors.append(s)
+                s = []
         else:
             n //= i
-            factors.append(i)
+            s.append(i)
+    if s:
+        factors.append(s)
     if n > 1:
-        factors.append(n)
+        if n == i and factors:
+            factors[-1].append(n)
+        else:
+            factors.append([n])
     return factors
 def main():
     lines = [x[:-1] for x in sys.stdin.readlines()[:-1]]
     for line in lines:
         linelen = len(line)
         pf = prime_factors(linelen)
-        if pf:
-            c = collections.Counter(pf)
-            factors = sorted([reduce(mul, x) for x in product(*[[base**x for x in seq] for base, seq in zip(c.keys(), [range(x+1) for x in c.values()])])])
-        else:
-            factors = [1, linelen]
-        for divisor in factors:
-            quotient = linelen//divisor
-            t = line[:divisor]*quotient
-            if t == line:
-                print(quotient)
-                break
+        if not pf:
+            pf = [[linelen]]
+        best = 1
+        for factors in pf:
+            for numparts in factors:
+                numparts = best*numparts
+                partlen = linelen//numparts
+                if line[:partlen]*numparts == line:
+                    best = numparts
+                else:
+                    break
+        print(best)
 main()
