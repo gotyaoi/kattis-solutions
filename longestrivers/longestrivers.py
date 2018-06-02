61bd201b390723e5b82e796b811261c95cd1e858
gotyaoi <gotyaoi@users.noreply.github.com>
2018-06-01 17:19:39 -0700

    Uses a simpler way of checking if a confluence has gone short.

--- longestrivers/longestrivers.py
+++ longestrivers/longestrivers.py
@@ -1,5 +1,5 @@
 import sys
-from heapq import heappush, heappop
+from heapq import heapify, heappush, heappop
 def main():
     numrivers, numconfluences = [int(x) for x in input().split()]
     data = sys.stdin.read().splitlines()
@@ -22,7 +22,7 @@ def main():
         else:
             to_check.append((i, dist))
     #max_len contains the best case length for each river, along with the river's index
-    max_len = [0]*numrivers
+    max_len = [None]*numrivers
     while to_check:
         i, dist = to_check.pop()
         if i >= numconfluences:
@@ -31,26 +31,22 @@ def main():
             to_check.extend([(x, dist+d) for x, d in upstream[i]])
     #state1 implicitely contains all confluences at the start
     #state2 is all rivers which are long and all confluences with short upstreams but which turn long
-    state2 = []
-    #state3 is all rivers which are short and all confluences with short upstreams and which stay short
-    state3 = set()
+    state2 = [(dist, i) for i, (conf, dist) in enumerate(points[numconfluences:], start=numconfluences)]
+    heapify(state2)
+    #state3 implicitely contains all rivers which are short and all confluences with short upstreams and which stay short
     #lengths is the length of each upstream point for each confluence
     lengths = [[] for _ in range(numconfluences)]
     ranks = [0]*numrivers
-    for i, (conf, dist) in enumerate(points[numconfluences:], start=numconfluences):
-        heappush(state2, (dist, i))
     for length, riverindex in sorted(max_len):
         try:
             priority, index = heappop(state2)
             while priority <= length:
-                state3.add(index)
+                #index goes to state3
                 downstreamindex = points[index][0]
                 if downstreamindex != -1:
                     lengths[downstreamindex].append(priority)
-                    conf = points[downstreamindex]
-                    ups = upstream[downstreamindex]
-                    if all([x[0] in state3 for x in ups]):
-                        conflen = min([x for x in lengths[downstreamindex]]) + conf[1]
+                    if len(upstream[downstreamindex]) == len(lengths[downstreamindex]):
+                        conflen = min([x for x in lengths[downstreamindex]]) + points[downstreamindex][1]
                         if conflen <= length:
                             index = downstreamindex
                             priority = conflen
